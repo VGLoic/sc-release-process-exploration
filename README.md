55f0098dc2143dc2b78b55d9a6c1b0e4082a304a
Loïc Vincent-Genod <37535451+VGLoic@users.noreply.github.com>
2024-01-05 13:22:52 +0900

    add changesets, derive next release version (#6)
    * add changesets, derive next release version
    
    * format
    
    * rename script
    
    * complete readme, update scripts name
    
    * update for consistency

--- README.md
+++ README.md
@@ -1,5 +1,133 @@
-# POC deployment process
+# Smart contracts release process exploration
 
-See branches for example:
-- `feature/counter-example`
-- `feature/external-lib-example`
+This repository illustrates a release process for the smart contracts.
+
+**The repository contains the compilation artifacts of the previous releases. Each release's artifacts are static and sufficient for deployment/interaction and verification on Etherscan.**
+
+**From the releases artifacts, a NPM package can be published containing the ABIs organized by contracts and release version.**
+
+## Motivation
+
+The goal is to satisfy in the simplest way possible the different actors:
+- Smart contract developers want to develop in an isolated way and not worrying about modifying artifacts of previous releases,
+- Smart contract "DevOps" want to have a simple way to create a freeze of the code at a certain point in time and create the associated artifacts. The artifacts should be sufficient for any later uses such as deployments, transactions, Etherscan verifications or generation of packages for downstream uses,
+- Backend and Frontend developers want to have a simple and safe way to interact with a contract given its release version.
+
+## Practical release process
+
+Once code is ready to be released,
+1. if not already there, add a changeset describing the changes and the version bump (see the *Release process details* section below for more details),
+```console
+yarn changeset
+```
+2. prepare the release artifacts that will define the release, the artifacts will be committed to this repository
+```console
+yarn release:prepare
+```
+3. Commit in a dedicated branch, create a PR and merge it.
+
+## Release process details
+
+The first need for the process is the management of the `version`. This repository uses [semver](https://semver.org/) versionning. In practice, version is managed here using [Changesets](https://github.com/changesets/changesets/tree/main).
+
+> [!NOTE]
+> Changesets let contributors declare how their changes should be released. 
+> 
+> Developers can add a *changeset* alongside their changes with a type *major*, *minor* or *patch* and a description. Before releasing, all changesets are consumed in order to derive the next release version.
+
+Once code is ready for a release,
+1. the *changesets* are consumed and the next release version is fixed,
+2. a compilation of the smart contracts is performed and artifacts are stored under a temporary folder `releases/tmp`,
+3. the `releases/tmp` is renamed using the release version: `releases/<release name>`, at this point the `releases` folder looks like
+```
+releases
+├── <release-name a>
+│   └── artifacts
+│       ├── build-info
+│       │   └── <build info file name>.json
+│       └── src
+│           ├── <contract-name>.sol
+│           │   └── <contract-name>.dbg.json
+│           │   └── <contract-name>.json
+│           └── ...
+└── <release-name b>
+    └── artifacts
+        ├── build-info
+        │   └── <build info file name>.json
+        └── src
+            ├── <contract-name>.sol
+            │   └── <contract-name>.dbg.json
+            │   └── <contract-name>.json
+            └── ...
+```
+
+In this case, the contracts are assumed to be under `src` folder.
+
+Each release folder is completely independent and contains all the informations for later operations.
+
+Once step 3. is performed, the rest of the process can be performed. This part is highly opinionated and is inspired from real-world needs.
+
+4. the delta between each releases is generated: the list of versions for each contract is built. If a contract has not changed since the previous release, it will be ignored. At this point the `releases` folder looks like
+```
+releases
+├── generated-delta
+|   │   ├── <contract-name>
+|   │   │   ├── <release-name a>.json // No change between release a and b
+|   │   │   ├── <release-name c>.json
+|   │   │   └── ...
+|   │   └── ...
+|   └── build-infos
+|       ├── <release-name a>.json
+|       ├── <release-name b>.json
+|       ├── <release-name c>.json
+|       └── ...
+├── <release-name a>
+│   └── artifacts
+│       ├── build-info
+│       │   └── <build info file name>.json
+│       └── src
+│           ├── <contract-name>.sol
+│           │   └── <contract-name>.dbg.json
+│           │   └── <contract-name>.json
+│           └── ...
+├── <release-name b>
+│   └── artifacts
+│       ├── build-info
+│       │   └── <build info file name>.json
+│       └── src
+│           ├── <contract-name>.sol
+│           │   └── <contract-name>.dbg.json
+│           │   └── <contract-name>.json
+│           └── ...
+└── <release-name c>
+    └── artifacts
+        ├── build-info
+        │   └── <build info file name>.json
+        └── src
+            ├── <contract-name>.sol
+            │   └── <contract-name>.dbg.json
+            │   └── <contract-name>.json
+            └── ...
+```
+5. the final step is to build the artifacts that will be exposed to downstream consumers through a NPM package. In this repository, only the ABIs are exposed, exposure is made as `json` file or as `TypeScript const`. This build uses [tsup](https://tsup.egoist.dev/) to realize the compilations. The generated artifacts are exposed in a git ignored `dist` folder with the following form
+```
+dist
+├── <contract-name>
+│   ├── <release-name a>.json
+│   ├── <release-name a>.js
+│   ├── <release-name a>.d.ts
+│   ├── <release-name c>.json
+│   ├── <release-name c>.js
+│   ├── <release-name c>.d.ts
+|   └── ...
+└── ...
+```
+
+
+## About deployment
+
+Deployment is considered as operations using one or multiple previous releases. It is therefore a consumer of the release process but it is not part of the release process.
+
+However, it impacts the content of a release as it must satisfy the needs of later deployment operations.
+
+Additional development may be later added on this.
---
+++ scripts/copy-compilation.ts
@@ -0,0 +1,200 @@
+import fs from "fs/promises";
+import getReleasePlan from "@changesets/get-release-plan";
+import { findLastRelease, semverStringToSemver, toResult } from "./utils";
+
+/**
+ * Copy the compilation artifacts from `releases/tmp` to `releases/<release name>`.
+ *
+ * This script must be run after at least one changeset for the new release has been added. Use `yarn changeset` to add a changeset.
+ * This script must be run after an isolated hardhat compilation using the `hardhat.config.release.ts` config file.
+ *
+ * The release name is retrieved from the changeset version.
+ *
+ * If there are no previous releases, it will create the first release by copying the `releases/tmp` folder to `releases/<release name>`,
+ * The next release is created by copying the `releases/tmp` folder to `releases/<release name>`,
+ *
+ * @dev Assumptions:
+ * - The `releases/tmp` contains the artifacts of the current release, it must have been created by a Hardhat compilation. See below for the exact expected structure.
+ * - The `releases/generated-delta` folder may exist, if it does, it contains the generated delta artifacts. Ignored in this script.
+ *
+ * @dev The `releases/tmp` folder has the following expected structure:
+ * ```
+ * releases/tmp
+ * └── artifacts
+ *   ├── build-info
+ *   │   └── <build info file name>.json
+ *   └── src
+ *      ├── <contract-name>.sol
+ *      │   └── <contract-name>.dbg.json
+ *      │   └── <contract-name>.json
+ *      └── ...
+ * ```
+ *
+ * @dev The `releases` folder will have the following structure after the script has run:
+ * ```
+ * releases
+ * ├── generated-delta // Ignored in this script
+ * ├── <release-name a>
+ * │   └── artifacts
+ * │       ├── build-info
+ * │       │   └── <build info file name>.json
+ * │       └── src
+ * │           ├── <contract-name>.sol
+ * │           │   └── <contract-name>.dbg.json
+ * │           │   └── <contract-name>.json
+ * │           └── ...
+ * └── <release-name b>
+ *     └── artifacts
+ *         ├── build-info
+ *         │   └── <build info file name>.json
+ *         └── src
+ *             ├── <contract-name>.sol
+ *             │   └── <contract-name>.dbg.json
+ *             │   └── <contract-name>.json
+ *             └── ...
+ *
+ */
+async function copyNewReleaseCompilation() {
+  const hasReleasesFolder = await fs.stat("./releases").catch(() => false);
+  if (!hasReleasesFolder) {
+    // Exit if there are no releases
+    console.error(
+      "❌ Releases folder has not been found at `./releases`. It should either alreay exist, or have been previously created by the hardhat compilation.",
+    );
+    process.exitCode = 1;
+    return;
+  }
+
+  const hasTmpFolder = await fs.stat("./releases/tmp").catch(() => false);
+  if (!hasTmpFolder) {
+    // Exit if there are no tmp folder
+    console.error(
+      "❌ Tmp folder has not been found at `./releases/tmp`. It should have been previously created by the hardhat compilation.",
+    );
+    process.exitCode = 1;
+    return;
+  }
+
+  const releasePlan = await getReleasePlan(process.cwd());
+  if (releasePlan.releases.length === 0 || releasePlan.releases.length > 1) {
+    const text =
+      releasePlan.releases.length === 0
+        ? "❌ No new release has been prepared. Please make sure to have added a changeset by using `yarn changeset` and try again."
+        : "❌ Unexpected error: multiple releases have been prepared. Only one is expected. Please check the release plan and try again.";
+    console.error(text);
+    await fs.rm("./releases/tmp", { recursive: true }).catch(() => {
+      console.warn(
+        `⚠️ An error occured while deleting the \`./releases/tmp\` folder. Please delete it manually and try again.`,
+      );
+    });
+    process.exitCode = 1;
+    return;
+  }
+
+  const newReleaseVersion = `v${releasePlan.releases[0].newVersion}`;
+  // Verify that the new release version is valid semver
+  if (!/^v\d+\.\d+\.\d+$/.test(newReleaseVersion)) {
+    console.error(
+      `❌ Unexpected error: the new release version ${newReleaseVersion} is not a valid semver. Please check the release plan and try again.`,
+    );
+    await fs.rm("./releases/tmp", { recursive: true }).catch(() => {
+      console.warn(
+        `⚠️ An error occured while deleting the \`./releases/tmp\` folder. Please delete it manually and try again.`,
+      );
+    });
+    process.exitCode = 1;
+    return;
+  }
+
+  // Check if there are previous releases by retrieving all the folders in `./releases` folder
+  // and filter out the `tmp` and `generated-delta` folders
+  const previousReleases = await fs
+    .readdir("./releases")
+    .then((releases) =>
+      releases.filter((r) => !["tmp", "generated-delta"].includes(r)),
+    );
+  if (previousReleases.length === 0) {
+    // If there are no previous releases
+    // We rename the `./releases/tmp` to `./releases/${newReleaseVersion}`
+    const renameResult = await toResult(
+      fs.rename("./releases/tmp", `./releases/${newReleaseVersion}`),
+    );
+    if (!renameResult.ok) {
+      console.error(
+        "❌ An error occured while creating the first release. The `./releases` folder will be deleted. Please check the error below and try again.",
+      );
+      console.error(renameResult.error);
+      await fs.rm("./releases", { recursive: true }).catch(() => {
+        console.warn(
+          "⚠️ An error occured while deleting the `./releases` folder. Please delete it manually and try again.",
+        );
+      });
+      process.exitCode = 1;
+      return;
+    }
+    console.log(
+      `✅ The next release ${newReleaseVersion} has been created. You can now run \`yarn release:generate-delta && yarn build\` to generate the artifacts to be distributed.`,
+    );
+  } else {
+    // 1. we verify that the release names are valid semver
+    const invalidReleases = previousReleases.filter(
+      (r) => !/^v\d+\.\d+\.\d+$/.test(r),
+    );
+    if (invalidReleases.length > 0) {
+      console.error(`❌ Invalid release names have been found. They should be valid semver.
+            The \`./releases/tmp\` folder will be deleted. Please inspect or delete manually the invalid releases and try again.
+            Invalid releases: ${invalidReleases.join(", ")}`);
+      await fs.rm("./releases/tmp", { recursive: true }).catch(() => {
+        console.warn(
+          `⚠️ An error occured while deleting the \`./releases/tmp\` folder. Please delete it manually and try again.`,
+        );
+      });
+      process.exitCode = 1;
+      return;
+    }
+    // 2. we verify that the last release is before the new release
+    const lastRelease = findLastRelease(previousReleases);
+    const newReleaseSemver = semverStringToSemver(newReleaseVersion);
+    if (
+      lastRelease.semver.major > newReleaseSemver.major ||
+      (lastRelease.semver.major === newReleaseSemver.major &&
+        lastRelease.semver.minor > newReleaseSemver.minor) ||
+      (lastRelease.semver.major === newReleaseSemver.major &&
+        lastRelease.semver.minor === newReleaseSemver.minor &&
+        lastRelease.semver.patch > newReleaseSemver.patch)
+    ) {
+      console.error(
+        `❌ The new release ${newReleaseVersion} is not after the last release ${lastRelease.name}. Please check the release plan and try again.`,
+      );
+      await fs.rm("./releases/tmp", { recursive: true }).catch(() => {
+        console.warn(
+          `⚠️ An error occured while deleting the \`./releases/tmp\` folder. Please delete it manually and try again.`,
+        );
+      });
+      process.exitCode = 1;
+      return;
+    }
+    // 2. we rename the `./releases/tmp` to `./releases/${newReleaseVersion}`
+    const renameResult = await toResult(
+      fs.rename("./releases/tmp", `./releases/${newReleaseVersion}`),
+    );
+    if (!renameResult.ok) {
+      console.error(
+        `❌ An error occured while creating the next release. The \`./releases/tmp\` folder will be deleted. Please check the error below and try again.`,
+      );
+      console.error(renameResult.error);
+      await fs.rm("./releases/tmp", { recursive: true }).catch(() => {
+        console.warn(
+          `⚠️ An error occured while deleting the \`./releases/tmp\` folder. Please delete it manually and try again.`,
+        );
+      });
+      process.exitCode = 1;
+      return;
+    }
+    console.log(
+      `✅ The next release ${newReleaseVersion} has been created. You can now run \`yarn release:generate-delta && yarn build\` to generate the artifacts to be distributed.`,
+    );
+  }
+}
+
+copyNewReleaseCompilation();
