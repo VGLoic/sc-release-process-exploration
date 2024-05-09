4ed5ee644f7bfb7f90227368fe34abdc978d9139
VGLoic <vincent.genod.loic@gmail.com>
2024-05-09 15:52:24 +0900

    add changes to contracts

--- src/Counter.sol
+++ src/Counter.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.13;
 
 import {IncrementOracle} from "./IncrementOracle.sol";
 
-contract Counter {
+contract Counter2 {
     uint256 public counter;
 
     function increment() public {
