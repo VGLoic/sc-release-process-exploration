b9d0e5a293d9174004dce18b5253cb39ac4bd20a
VGLoic <vincent.genod.loic@gmail.com>
2024-08-16 10:49:29 +0200

    change args \

--- .github/release.sh
+++ .github/release.sh
@@ -5,7 +5,7 @@ RELEASE_TAG_WITHOUT_PREFIX=$(cat package.json | jq -r '.version')
 
 echo "Publishing release $RELEASE_TAG_WITHOUT_PREFIX"
 
-yarn hardhat soko push "v$RELEASE_TAG_WITHOUT_PREFIX" && echo "Successfully pushed release artifact" || echo "Failed to push release, we assume here that this is because the release already exists. We need to improve this!"
+yarn hardhat soko push --release "v$RELEASE_TAG_WITHOUT_PREFIX" && echo "Successfully pushed release artifact" || echo "Failed to push release, we assume here that this is because the release already exists. We need to improve this!"
 
 echo "Downloading release artifacts"
 
